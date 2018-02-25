68d0305ba41941b0cef8846c4924aa3a78b82054
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-02-25 08:51:28 +0000

    Add trails to planets

--- src/gravity_sim/Window.java
+++ src/gravity_sim/Window.java
@@ -1,6 +1,8 @@
 package gravity_sim;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 import javax.swing.*;
@@ -11,6 +13,7 @@ public class Window extends JComponent {
 	private static final long serialVersionUID = 1L; 
 	
 	public static CopyOnWriteArrayList<Planet> planetList = new CopyOnWriteArrayList<Planet>();
+	public static CopyOnWriteArrayList<Point> planetTrails = new CopyOnWriteArrayList<Point>();
 	public static int[] windowSize = {1920, 1080};
 	
 	public static void main(String[] a) throws InterruptedException {
@@ -22,6 +25,24 @@ public class Window extends JComponent {
 		f.add(animation);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	    f.addKeyListener(new KeyListener() {
+	        @Override
+	        public void keyPressed(KeyEvent e) {
+	            if (e.getKeyCode() == 82) {
+	            	// "r" key pressed
+	            	planetTrails.clear();
+	            }
+	        }
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+			}
+	    });
 		
 		// Main loop of program
 		while (true) {
@@ -80,7 +101,21 @@ public class Window extends JComponent {
 	/*
 	 * Paint the planets onto the screen
 	 */
-	public void paintComponent(Graphics g){
+	public void paintComponent(Graphics g) {
+		
+		// Paint the trails into existence
+		g.setColor(Color.BLACK);
+		for(Point t: planetTrails) {
+			g.fillOval(
+					t.x, 
+					t.y, 
+					2, 
+					2
+					);
+		}
+		while (planetTrails.size() > 50000) {
+			planetTrails.remove(0);
+		}
 		
 		// Paint each planet into existence
 		for(Planet p: planetList){
@@ -92,7 +127,15 @@ public class Window extends JComponent {
 					(int) PlanetPhysics.scaleToPixel(position[1]) - radius, 
 					2*radius, 
 					2*radius
+			
+			// Add planet trail
+			double[] position = p.getPos();
+			Point l = new Point();
+			l.setLocation(
+					PlanetPhysics.scaleToPixel(position[0]), 
+					PlanetPhysics.scaleToPixel(position[1])
 					);
+			planetTrails.add(l);
 		}
 	}
 }
